ae33b11727987c0a0f05d9057a1692ddf306ca0a
SkittleSiz <ichemp13@gmail.com>
2020-08-16 20:31:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import '@vkontakte/vkui/dist/vkui.css';
+
+const App = () => {
+	const [activePanel, setActivePanel] = useState('home');
+	const [fetchedUser, setUser] = useState(null);
+	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+
+	useEffect(() => {
+		bridge.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				const schemeAttribute = document.createAttribute('scheme');
+				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+				document.body.attributes.setNamedItem(schemeAttribute);
+			}
+		});
+		async function fetchData() {
+			const user = await bridge.send('VKWebAppGetUserInfo');
+			setUser(user);
+			setPopout(null);
+		}
+		fetchData();
+	}, []);
+
+	const go = e => {
+		setActivePanel(e.currentTarget.dataset.to);
+	};
+
+	return (
+		<div className="App">
+		  <h1>Hello CodeSandbox</h1>
+		  <h2>Start editing to see some magic happen!</h2>
+		</div>
+	);
+}
+
+export default App;
+
